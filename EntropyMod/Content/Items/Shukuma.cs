854f19cddc24606367efef38bc4f77fbd74476a4
omarlectro <67567739+omarlectro@users.noreply.github.com>
2025-06-24 08:08:25 -0500

    agrega arma mele "Shukuma"

---
+++ EntropyMod/Content/Items/Shukuma.cs
@@ -0,0 +1,49 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace EntropyMod.Content.Items
+{
+    public class Shukuma : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.damage = 35;
+            Item.DamageType = DamageClass.Melee;
+            Item.width = 30;
+            Item.height = 28;
+            Item.useTime = 20;
+            Item.useAnimation = 20;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.knockBack = 5f;
+            Item.value = Item.sellPrice(silver: 75);
+            Item.rare = ItemRarityID.Blue;
+            Item.UseSound = SoundID.Item1;
+            Item.autoReuse = true;
+            Item.crit = 10;
+            Item.useTurn = true; // Puedes agregar esto para mejorar el control al atacar
+        }
+
+        public override void MeleeEffects(Player player, Rectangle hitbox)
+        {
+            if (Main.rand.NextBool(2))
+            {
+                int dustIndex = Dust.NewDust(new Vector2(hitbox.X, hitbox.Y), hitbox.Width, hitbox.Height, DustID.Blood);
+                Dust dust = Main.dust[dustIndex];
+                dust.velocity *= 0.3f;
+                dust.scale = 1.2f;
+                dust.noGravity = true;
+            }
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ModContent.ItemType<EntropyScrap>(), 4);
+            recipe.AddIngredient(ItemID.Ruby, 2);
+            recipe.AddTile(TileID.Anvils);
+            recipe.Register();
+        }
+    }
+}
